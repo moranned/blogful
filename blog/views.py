1cb01c452efa98991fc16da03e60ffcd8956ec51
moranned <ned.moran@gmail.com>
2015-02-25 16:40:41 +0000

    Added new features

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,78 @@
+import mistune
 from flask import render_template
+from flask import request, redirect, url_for
 
 from blog import app
 from .database import session
 from .models import Post
 
 @app.route("/")
-def posts():
+@app.route("/page/<int:page>")
+def posts(page=1, paginate_by=10):
+    # Zero-indexed page
+    page_index = page - 1
+
+    count = session.query(Post).count()
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) / paginate_by + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
     posts = session.query(Post)
     posts = posts.order_by(Post.datetime.desc())
-    posts = posts.all()
+    posts = posts[start:end]
+
     return render_template("posts.html",
-        posts=posts
+        posts=posts,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
     )
+
+@app.route("/post/<id>")
+def post(id):
+  id = int(id) + 1
+  post = session.query(Post).filter(Post.id == id).all()[0]
+  return render_template("post.html", post=post)
+
+@app.route("/post/<id>/edit", methods=["GET"])
+def edit(id):
+  id = int(id) + 1
+  post = session.query(Post).filter(Post.id == id).all()[0]
+  return render_template("edit_post.html", post=post)
+
+@app.route("/post/<id>/delete", methods=["GET"])
+def delete_post(id):
+  id = int(id) + 1
+  post = session.query(Post).filter(Post.id == id).all()[0]
+  session.delete(post)
+  session.commit()
+  return redirect(url_for("posts"))
+
+@app.route("/post/<id>/edit", methods=["POST"])
+def edit_post(id):
+  post = Post(
+    title=request.form["title"],
+    content=mistune.markdown(request.form["content"]),
+  )
+  session.add(post)
+  session.commit()
+  return redirect(url_for("posts"))
+
+@app.route("/post/add", methods=["GET"])
+def add_post_get():
+    return render_template("add_post.html")
+
+@app.route("/post/add", methods=["POST"])
+def add_post_post():
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
